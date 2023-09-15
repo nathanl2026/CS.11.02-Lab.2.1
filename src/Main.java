d6b3a7fac1525c8e17ad406036e00a3bdf9cc064
nathanlau <nathanl2026@student.cis.edu.hk>
2023-09-15 17:47:11 +0800

    Added text in Main

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,47 @@ public class Main {
     }
 
     // 1. add
+    public static int add(int a, int b) {
+        return a+b;
+    }
     
     // 2. add
 
+    public static int add(int a, int b, int c, int d) {
+        return add(add(add(a, b),c),d);
+    }
+
     // 3. morningGreeting
+    public static String morningGreeting(String name) {
+        return "早上好, " + name + "!";
+}
 
     // 4. afternoonGreeting
+    public static String afternoonGreeting(String name) {
+        return "下午好, " + name + "!";
+    }
 
     // 5. triple
+    public static String triple(String text) {
+        return "\"" + text + text + text + "\"";
+    }
 
     // 6. half
+    public static int half(int number) {
+        return number / 2;
+    }
 
     // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double number) {
+       return (int) Math.round(number);
+
+    }
+
 
     // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger(double number) {
+        return (int) Math.round(number);
+    }
+
 
 }
